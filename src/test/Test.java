7d622bc4c240c8e73857e09b65ea6d8482018ecb
Iusheva Evgeniia <evgeniia.iusheva@gmail.com>
2023-11-27 11:32:40 +0100

    init

---
+++ src/test/Test.java
@@ -0,0 +1,10 @@
+package test;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Mur-mur");
+        System.out.println("miau");
+        System.out.println("miau2");
+    }
+
+}
